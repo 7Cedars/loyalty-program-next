40293f6dc7f15e9f6802e216588e051c2625cfeb
Seven Cedars <Cedars7@proton.me>
2024-03-12 11:04:55 +0000

    fixed bug not showing loading sign on chooseprogram

--- src/app/vendor/components/ChooseProgram.tsx
+++ src/app/vendor/components/ChooseProgram.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LoyaltyProgram} from "@/types";
+import { LoyaltyProgram, Status} from "@/types";
 import { TitleText } from "../../ui/StandardisedFonts";
 import { useEffect, useState} from "react";
 import Image from "next/image";
@@ -13,24 +13,47 @@ import { Log } from "viem";
 import { parseContractLogs } from "@/app/utils/parsers";
 
 export default function ChooseProgram()  {
-  const { status, loyaltyPrograms, fetchPrograms } = useLoyaltyPrograms()
+  const { status: statusUseLoyaltyPrograms, loyaltyPrograms, fetchPrograms } = useLoyaltyPrograms()
+  const [ statusFetchingAddresses, setStatusFetchingAddresses ] = useState<Status>("isIdle")
+  const [ status, setStatus ] = useState<Status>("isIdle")
   const [ addresses, setAddresses ] = useState<LoyaltyProgram[] | undefined>() 
   const { address } = useAccount() 
   const publicClient = usePublicClient(); 
   const dispatch = useDispatch() 
 
   const getLoyaltyProgramAddresses = async () => {
-
-    const loggedAdresses: Log[] = await publicClient.getContractEvents( { 
-      abi: loyaltyProgramAbi, 
-        eventName: 'DeployedLoyaltyProgram', 
-        args: {owner: address}, 
-        fromBlock: 5200000n
-    });
-
-    const loyaltyProgramAddresses = parseContractLogs(loggedAdresses)
-    setAddresses(loyaltyProgramAddresses)
+    setStatusFetchingAddresses("isLoading")
+    try { 
+        const loggedAdresses: Log[] = await publicClient.getContractEvents( { 
+          abi: loyaltyProgramAbi, 
+            eventName: 'DeployedLoyaltyProgram', 
+            args: {owner: address}, 
+            fromBlock: 5200000n
+        });
+        const loyaltyProgramAddresses = parseContractLogs(loggedAdresses)
+        setAddresses(loyaltyProgramAddresses)
+        setStatusFetchingAddresses("isSuccess")
+      } catch (error) { 
+        setStatusFetchingAddresses("isError")
+        console.log(error)
+      }
   }
+  
+  // updating status 
+  useEffect(() => {
+    if (
+      statusUseLoyaltyPrograms == "isLoading" || 
+      statusFetchingAddresses == "isLoading"
+    ) setStatus("isLoading")
+    if (
+      statusUseLoyaltyPrograms == "isError" || 
+      statusFetchingAddresses == "isError"
+    ) setStatus("isError")
+    if (
+      statusUseLoyaltyPrograms == "isSuccess" && 
+      statusFetchingAddresses == "isSuccess"
+    ) setStatus("isSuccess")
+  }, [ statusUseLoyaltyPrograms, statusFetchingAddresses ])
 
   useEffect(() => {
     if (address && !addresses)  getLoyaltyProgramAddresses() 
@@ -39,13 +62,13 @@ export default function ChooseProgram()  {
 
   useEffect(() => {
     if (
-      status == "isSuccess" && 
+      statusUseLoyaltyPrograms == "isSuccess" && 
       loyaltyPrograms && 
       loyaltyPrograms.length == 1 && 
       loyaltyPrograms.findIndex(loyaltyProgram => loyaltyProgram.metadata) !== -1
       ) 
       dispatch(selectLoyaltyProgram(loyaltyPrograms[0]))
-  }, [status, loyaltyPrograms])
+  }, [statusUseLoyaltyPrograms, loyaltyPrograms])
 
   return (
      <div className='w-full h-full flex flex-col items-center justify-center' >
